049d12e7b277b5c308bcb3c5329ae0d47cdf0715
Jonathan Channon <jonathan.channon@gmail.com>
2017-06-15 16:56:11 +0100

    added custom logger

--- src/Fluentd/FluentdHandler.cs
+++ src/Fluentd/FluentdHandler.cs
@@ -1,11 +1,13 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog.Events;
-using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
+using Serilog.Parsing;
 
 namespace Serilog.Sinks.Fluentd.Core.Fluentd
 {
@@ -41,8 +43,7 @@ namespace Serilog.Sinks.Fluentd.Core.Fluentd
         {
             using (var sw = new StringWriter())
             {
-                var formatter = new CompactJsonFormatter();
-                formatter.Format(message, sw);
+                this.Format(message, sw);
 
                 var serialized = $"[\"{this.Settings.Tag}\",{message.Timestamp.ToUnixTimeSeconds()},{sw.ToString()}]";
                 var encoded = Encoding.UTF8.GetBytes(serialized);
@@ -65,6 +66,66 @@ namespace Serilog.Sinks.Fluentd.Core.Fluentd
                     semaphore.Release();
                 }
             }
+        }
+
+        private void Format(LogEvent logEvent, StringWriter output)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            output.Write("{\"@t\":\"");
+            output.Write(logEvent.Timestamp.UtcDateTime.ToString("O"));
+            output.Write("\",\"@mt\":");
+            JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
+
+            var tokensWithFormat = logEvent.MessageTemplate.Tokens
+                .OfType<PropertyToken>()
+                .Where(pt => pt.Format != null);
+
+            // Better not to allocate an array in the 99.9% of cases where this is false
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (tokensWithFormat.Any())
+            {
+                output.Write(",\"@r\":[");
+                var delim = "";
+                foreach (var r in tokensWithFormat)
+                {
+                    output.Write(delim);
+                    delim = ",";
+                    var space = new StringWriter();
+                    r.Render(logEvent.Properties, space);
+                    JsonValueFormatter.WriteQuotedJsonString(space.ToString(), output);
+                }
+                output.Write(']');
+            }
+
+            output.Write(",\"@l\":\"");
+            output.Write(logEvent.Level);
+            output.Write('\"');
+
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"@x\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
+            }
+
+            foreach (var property in logEvent.Properties)
+            {
+                var name = property.Key;
+                if (name.Length > 0 && name[0] == '@')
+                {
+                    // Escape first '@' by doubling
+                    name = '@' + name;
+                }
+
+                output.Write(',');
+                JsonValueFormatter.WriteQuotedJsonString(name, output);
+                output.Write(':');
+                new JsonValueFormatter(typeTagName: "$type").Format(property.Value, output);
+            }
+
+            output.Write('}');
         }
 
         private void Disconnect()
